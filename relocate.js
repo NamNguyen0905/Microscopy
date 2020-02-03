7f32d4f07ce01194c392a548b8b2ae266a09c604
Nam Nguyen <namnguyen0905@outlook.com>
2020-02-03 18:03:56 -0500

    Add script to relocate files

--- relocate.js
+++ relocate.js
@@ -8,35 +8,17 @@ const path = require("path");
 let buffer = fs.readFileSync(__dirname + "/data");
 var users = JSON.parse(buffer);
 
-function moveFileTo(file, dest) {
-  // get the file name of the path
-  const fileName = path.basename(file);
-
-  // combine the path of destination directory and the filename
-  const destPath = path.join(dest, fileName);
-
-  try {
-    await fs.rename(file, destPath);
-  } catch (err) {
-    if (err.code === "EXDEV") {
-      // we need to copy if the destination is on another parition
-      await copyFile(file, destPath);
-
-      // delete the old file if copying was successful
-      await unlink(file);
-    } else {
-      // re throw the error if it is another error
-      throw err;
-    }
-  }
-}
-
 for (let i = 0; i < users.length; i++) {
-  if (!fs.existsSync("./A7/" + users.y)) {
-    fs.mkdir("./0/" + users.y, callback);
+  if (!fs.existsSync("./0/" + users[i].y)) {
+    fs.mkdir("./0/" + users[i].y, error => {});
   }
-  moveFileTo(
-    "./0/" + users.y + "/" + users.name + ".jpg",
-    "./0/" + users.y + "/" + users.x + ".jpg"
+
+  fs.rename(
+    "./A7/" + users[i].name + ".jpg",
+    "./0/" + users[i].y + "/" + users[i].x + ".jpg",
+    function(err) {
+      if (err) throw err;
+      console.log("Move complete.");
+    }
   );
 }
