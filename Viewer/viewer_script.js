6894392a387b51877abca9b77eaeacc4e6af02ef
Nam Nguyen <namnguyen0905@outlook.com>
2020-03-16 11:45:18 -0400

    Add sidebar, feature to delete marker and edit notes in the markers

--- Viewer/viewer_script.js
+++ Viewer/viewer_script.js
@@ -1,16 +1,15 @@
 $(document).ready(function() {
   let page = localStorage.getItem("slide");
-  console.log(page);
 
   //Get URL to given slide
   let slide = "../slides/" + page + "/{z}/{y}/{x}.jpg";
   let map = L.map("map", {
     center: [0, 0],
-    zoom: 3
+    zoom: 2
   });
 
   let layer = new L.tileLayer(slide, {
-    minZoom: 3,
+    minZoom: 2,
     maxZoom: 8,
     continousWorld: false,
     noWrap: true
@@ -34,32 +33,114 @@ $(document).ready(function() {
     width: 225
   }).addTo(map);
 
+  //Side bar
+  let sidebar = L.control.sidebar("sidebar").addTo(map);
+
   //Get click_coordinate
   let coord;
+  let markers = [];
+  let remove_coordinate;
   map.on("click", function(e) {
-    coord = e.latlng;
-    let lat = coord.lat;
-    let lng = coord.lng;
     console.log(
-      "You clicked the map at latitude: " + lat + " and longitude: " + lng
+      "You clicked the map at latitude: " +
+        e.latlng.lat +
+        " and longitude: " +
+        e.latlng.lng
     );
 
-    //L.marker([lat, lng]).addTo(map);
+    let marker = L.marker([e.latlng.lat, e.latlng.lng])
+      .bindPopup("")
+      .addTo(map);
+
+    marker.on("click", function(e) {
+      remove_coordinate = e.latlng;
+      console.log(
+        "Your marker is at latitude: " +
+          e.latlng.lat +
+          " and longitude: " +
+          e.latlng.lng
+      );
+    });
+
+    markers.push(marker);
   });
 
-  //Sample marker
-  L.marker([7.36246686553575, -49.5703125])
-    .bindPopup("Test")
+  //Sample markers
+  let marker_1 = L.marker([7.36246686553575, -49.5703125])
+    .bindPopup("Test 1")
     .addTo(map);
-  console.log(map.getBounds());
+
+  let marker_2 = L.marker([8.407168163601076, -4.570312500000001])
+    .bindPopup("Test 2")
+    .addTo(map);
+
+  marker_1.on("click", function(e) {
+    remove_coordinate = e.latlng;
+    console.log(
+      "Your marker is at latitude: " +
+        e.latlng.lat +
+        " and longitude: " +
+        e.latlng.lng
+    );
+  });
+
+  marker_2.on("click", function(e) {
+    remove_coordinate = e.latlng;
+    console.log(
+      "Your marker is at latitude: " +
+        e.latlng.lat +
+        " and longitude: " +
+        e.latlng.lng
+    );
+  });
+  markers.push(marker_1);
+  markers.push(marker_2);
+
+  // miniMap.on("click", function(e) {
+  //   coord = e.latlng;
+  //   let lat = coord.lat;
+  //   let lng = coord.lng;
+  //   console.log(
+  //     "You clicked the map at latitude: " + lat + " and longitude: " + lng
+  //   );
+  // });
 
   //Disable dragging on minZoom
   map.on("zoomend", function() {
     var zoomlevel = map.getZoom();
-    if (zoomlevel > 3) map.dragging.enable();
+    if (zoomlevel > 2) map.dragging.enable();
     else {
-      map.setView([0, 0]);
+      map.setView(map.getCenter());
       map.dragging.disable();
+    }
+  });
+
+  //Function to remove marker
+  let remove_marker;
+  $('input[type="button"]').on("click", function() {
+    for (let i = 0; i < markers.length; i++)
+      if (markers[i]._latlng == remove_coordinate) {
+        remove_marker = markers[i];
+        markers.splice(i, 1);
+        break;
+      }
+
+    remove_marker.remove();
+  });
+
+  //Function to edit annotation
+  let edit_marker;
+  $('input[type="text"]').keypress(function(e) {
+    for (let i = 0; i < markers.length; i++)
+      if (markers[i]._latlng == remove_coordinate) {
+        edit_marker = markers[i];
+        break;
+      }
+    if (!e) e = window.event;
+    let keyCode = e.keyCode || e.which;
+    if (keyCode == "13") {
+      edit_marker._popup.setContent($('input[type="text"]').val());
+      return false;
     }
   });
 });
--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(function() {
   $("img").on("click", function() {
     //Store slide name when click on the image
     let slide_name = $(this).attr("id");
-    console.log(slide_name);
 
     localStorage.setItem("slide", slide_name);
   });
